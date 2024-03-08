98ea8cccfe2e0686854976e64596b40eea93454e
4118 Рыков В.С <vladislav.ok.07@mail.ru>
2024-03-08 03:05:52 +0300

    comm9

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const { data: session } = useSession();
   const searchParams = useSearchParams();
   const message = searchParams.get('message');
   const router = useRouter();
